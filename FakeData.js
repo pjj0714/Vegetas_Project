0bc02641ac42a0b428377bd95539ed2556fd83dc
pjj0714 <pjj1587@gmail.com>
2019-02-04 23:41:51 +0900

    사진 FakeData

---
+++ FakeData.js
@@ -0,0 +1,9 @@
+export default FakeData = [
+    "http://news.tongplus.com/site/data/img_dir/2017/02/21/2017022101681_0.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBqoubwGu2dJNWJ27HcpmxlRNCX-VX4z943X1ACJZSy8Mz6_mZ",
+    'https://previews.123rf.com/images/robynmac/robynmac0901/robynmac090100015/4191649-%EB%B3%B6%EC%9D%80-%EC%95%BC%EC%B1%84%EC%99%80-%ED%98%BC%ED%95%A9-%EB%90%9C-%EC%95%BC%EC%B1%84%EC%99%80-%EC%B9%98%ED%82%A8-%EC%83%90%EB%9F%AC%EB%93%9C-%EB%A7%9B%EC%9E%88%EB%8A%94-%EA%B1%B4%EA%B0%95%ED%95%9C-%EC%8B%9D%EC%83%9D%ED%99%9C-.jpg',
+    'https://t1.daumcdn.net/cfile/tistory/25657F3C58FD95E825',
+    'http://cdnweb01.wikitree.co.kr/webdata/editor/201503/10/img_20150310151032_ccdb534d.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwBVaJK2HQy_d_5eSiRZ7BfLpqIM3oRxxP3PA9-EF-LkLiYE0g'
+  ];
+  
